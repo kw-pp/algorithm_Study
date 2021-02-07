38b90fac1d33ada2d8b887623b64ec2fb20fb46e
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-02-08 01:20:35 +0900

    Create 20210208_boj_9663_nqueen(메모리초과).c
    n = 12까지 잘 출력되지만 13,14부터 메모리 문제인지 출력 안됨
    메모리, 시간을 줄일 수 있는 방법을 찾아보기

---
+++ hiseoung/20210208_boj_9663_nqueen(메모리초과).c
@@ -0,0 +1,94 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int cnt = 0;
+
+void NQueen(int N, int r, int** arr) {
+    for (int j = 0; j < N; j++) {
+
+        if (arr[r][j] == 0 && j == N - 1) {
+            //printf("%d %d 백트래킹\n", r, j);
+            return;
+        }
+
+        if (arr[r][j] == 0) {
+            continue;
+        }
+
+
+        if (arr[r][j] == 1) {
+            if (r == N - 1) {
+                cnt++;
+                //printf("%d %d NQueen찾기 성공!!!\n", r, j);
+                return 1;
+            }
+
+            int** b;
+            b = (int**)malloc(sizeof(int*) * N);
+            for (int i = 0; i < N; i++) {
+                b[i] = (int*)malloc(sizeof(int) * N);
+                for (int j = 0; j < N; j++) b[i][j] = arr[i][j];
+            }
+
+            int** b;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) b[i][j] = arr[i][j];
+            }
+            for (int z = 0; r + z < N; z++) {
+
+
+                if (r + z < N) {
+                    b[r + z][j] = 0;
+                }
+                if (r + z < N && j + z < N) {
+                    b[r + z][j + z] = 0;
+                }
+                if (j - z >= 0) {
+                    b[r + z][j - z] = 0;
+                }
+
+            }
+            //for (int c = 0; c < N; c++) {
+            //    for (int l = 0; l < N; l++) {
+
+            //        printf("%d ", b[c][l]);
+
+            //    }
+            //    printf("\n");
+
+            //}
+
+            NQueen(N, r + 1, b);
+        }
+
+    }
+    return 0;
+}
+
+int main(void)
+{
+
+    int N = 0;
+    scanf("%d", &N);
+    int r = 0;
+
+    int** arr;
+    arr = (int**)malloc(sizeof(int*) * N);
+    for (int i = 0; i < N; i++) {
+        arr[i] = (int*)malloc(sizeof(int) * N);
+        for (int j = 0; j < N; j++) arr[i][j] = 1;
+    }
+
+    int** b;
+    b = (int**)malloc(sizeof(int*) * N);
+    for (int i = 0; i < N; i++) {
+        b[i] = (int*)malloc(sizeof(int) * N);
+    }
+
+
+
+    NQueen(N, r, arr);
+    printf("%d", cnt);
+
+}
