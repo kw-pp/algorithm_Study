94d0563b0439ec4d9d3ef579c139e074f50cb841
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-03-25 16:09:19 +0900

    Create 20210325_boj_9251_lcs.py

---
+++ hiseoung/BOJ/20210325_boj_9251_lcs.py
@@ -0,0 +1,14 @@
+import sys
+
+a = sys.stdin.readline().strip()
+b = sys.stdin.readline().strip()
+dp = [[0 for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
+
