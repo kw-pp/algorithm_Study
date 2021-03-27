800cb5ecbb8f861847f4c322d538117808354e46
uniLee1119 <49422373+uniLee1119@users.noreply.github.com>
2021-03-27 15:42:12 +0900

    Update 20210323_9251_LCS.py

--- yoonhee/BOJ/20210323_9251_LCS.py
+++ yoonhee/BOJ/20210323_9251_LCS.py
@@ -1,13 +1,16 @@
-def lcs(x,y):
-    prev = [0]*len(a)
-    for i,r in enumerate(a):
-        currnet = []
-        for j,c in enumerate(b):
-            if r == c:
-                e = prev[j-1] + if i*j > 0 else 1
-                else:
-                    e = max(prev[j] if i > 0 else 0, current[-1] if j > 0 else 0)
+s1 = list(str(input()))
+s2 = list(str(input()))
 
-                current.append(e)
-            prev = current
-    return current[-1]
+# 2차원 배열 생성
+arr = [[0 for _ in range(len(s1) + 1)] for _ in range(len(s2) +1)]
+#result = 0
+
+for i in range(len(s1)):
+    for j in range(len(s2)):
+        # 두 배열의 원소가 같은 경우
+        if s1[i] == s2[j]:
+            arr[j+1][i+1] = arr[j][i] + 1
+        else:
+            arr[j+1][i+1]= max(arr[j+1][i], arr[j][i+1])
+            
+print(arr[-1][-1])
