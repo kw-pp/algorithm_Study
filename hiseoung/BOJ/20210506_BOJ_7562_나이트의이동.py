f241a7f92bfd38c02d3b7ceebdaf52a6b42a6926
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-05-07 00:13:22 +0900

    Update 20210506_BOJ_7562_나이트의이동.py

--- hiseoung/BOJ/20210506_BOJ_7562_나이트의이동.py
+++ hiseoung/BOJ/20210506_BOJ_7562_나이트의이동.py
@@ -12,31 +12,38 @@ for i in range(t):
 
     is_visited = [[False] * n for _ in range(n)]
     q = deque()
-    q.append((px, py))
+    if px == tx and py == ty:
+        pass
+    else:
+        q.append((px, py))
     is_visited[px][py] = True
     count = 0
     breaker = False
-    # dx = [1, 2, 2, 1, -1, -2, -2, -1]
-    # dy = [2, 1, -1, -2, -2, -1, 1, 2]
+
     while q:
         count += 1
-        x, y = q.popleft()
-        for a, b in (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2):
-            if 0 <= x+a < n and 0 <= y+b < n and not is_visited[x+a][y+b]:
-                if x + a == tx and y + b == ty:
-                    print("정답 찾음")
-                    breaker = True
-                    break
-                q.append((x+a, y+b))
-                is_visited[x+a][y+b] = True
+        for _ in range(len(q)):
+            x, y = q.popleft()
+            for a, b in (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2):
+                if 0 <= x+a < n and 0 <= y+b < n and not is_visited[x+a][y+b]:
+                    if x + a == tx and y + b == ty:
+                        # print("정답 찾음")
+                        # print(count)
+                        breaker = True
+                        break
+                    q.append((x+a, y+b))
+                    is_visited[x+a][y+b] = True
 
             if breaker:
                 break
-
+        if breaker:
+            break
     answer.append(count)
 
-for i in answer:
-    print(i)
+for i in range(t):
+    print(answer[i])
+
+
 
 
 
