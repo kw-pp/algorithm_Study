8d8f1bddf7f22bf591e246e2d2bf36699751235e
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-07-28 22:44:39 +0900

    Update Programmers

---
+++ hiseoung/PROGRAMERS/20210728_PROGRAMMERS_타겟 넘버(level2).py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def solution(numbers, target):
+    '''
+    출처 : https://programmers.co.kr/learn/courses/30/lessons/43165
+    문제 유형 : DFS & BFS
+    새롭게 알게 된 내용
+    - DFS는 스택을 활용, BFS는 큐를 활용한 탐생 방법
+    - 일반적으로 DFS보다 BFS의 속도가 더 빠르다
+    '''
+
+    # TODO : DFS를 이용해 문제 해결
+
+    return answer
+
+def dfs(value, li, visited):
+    for i in range(len(li)):
+
+
