fe49286f4c4597e144fe9809f2a5781944790e1a
Doyeon <eded0902@naver.com>
2021-07-31 11:37:13 +0900

    Create 20210731_타겟 넘버(level2).py

---
+++ doyeon/PROGRAMMERS/DFS&BFS/20210731_타겟 넘버(level2).py
@@ -0,0 +1,26 @@
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
+
+# 다른 사람 풀이
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
