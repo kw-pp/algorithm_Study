9e01107c627276a91b6f42d13ad4be0f4a0bd213
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-08-05 01:21:28 +0900

    Update

--- hiseoung/PROGRAMERS/20210726_PROGRAMMERS_정수삼각형(level3).py
+++ hiseoung/PROGRAMERS/20210726_PROGRAMMERS_정수삼각형(level3).py
@@ -1,3 +1,5 @@
+import sys
+
 def solution(triangle):
     '''
     출처 : https://programmers.co.kr/learn/courses/30/lessons/43105
@@ -19,3 +21,12 @@ def solution(triangle):
                     dp[i][j] = triangle[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j])
 
     return max(dp[tri_len-1])
+
+n = int(sys.stdin.readline())
+li = [[] for _ in range(n)]
+for i in range(0, n):
+    temp = list(map(int, input().split()))
+    li[i] = temp
+    
+print(solution(li))
+
