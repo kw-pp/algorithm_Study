1bb00149205701134e8c8451e563319ff423d680
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-09-20 18:30:30 +0900

    Update boj

--- hiseoung/BOJ/20210918_BOJ_1987_알파벳.py
+++ hiseoung/BOJ/20210918_BOJ_1987_알파벳.py
@@ -10,17 +10,17 @@ def solution(x, y ,count, arr):
     유형 : 그래프 탐색
     - 처음에 BFS로 접근했는데 경로상의 중복을 체크할 수 가 없음 -> DFS로 짜는게 나을 듯
     - DFS로 짜서 답은 맞추는데 시간초과...
+    - bfs / dfs + 백트래킹기법이 추가되어야 풀이가 가능하다고 한다..
+
     '''
     global Max, r, c
-    if count > Max:
-        Max = count
+    Max = max(count, Max)
+
     for a, b in zip(dx, dy):
-        if 0 <= x + a < r and 0 <= y + b < c and not is_visited[ord(arr[x + a][y + b])-65]:
-            count += 1
-            is_visited[ord(arr[x + a][y + b])-65] = 1
-            solution(x+a, y+b, count, arr)
-            is_visited[ord(arr[x + a][y + b])-65] = 0
-            count -= 1
+        if 0 <= x + a < r and 0 <= y + b < c and not is_visited[ord(arr[x + a][y + b])]:
+            is_visited[ord(arr[x + a][y + b])] = 1
+            solution(x+a, y+b, count + 1, arr)
+            is_visited[ord(arr[x + a][y + b])] = 0
 
 
 
@@ -28,10 +28,10 @@ r, c = map(int, sys.stdin.readline().split())
 arr = []
 for i in range(r):
     arr.append(list(sys.stdin.readline().strip()))
-is_visited = [0] * 27
+is_visited = [0] * 91
 Max = float('-inf')
 count = 1
-is_visited[ord(arr[0][0])-65] = 1
+is_visited[ord(arr[0][0])] = 1
 solution(0, 0, count, arr)
 print(Max)
 
@@ -65,3 +65,5 @@ print(Max)
 # for i in range(5):
 #     for j in range(5):
 #         a.add(arr[i][j])
+
+
