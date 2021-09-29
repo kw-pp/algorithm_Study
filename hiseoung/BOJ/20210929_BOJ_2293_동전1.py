2a0f14edfc1bc726b62e8ad409cff08831e85dac
hiseoung <anhisg4104@gmail.com>
2021-09-29 23:19:18 +0900

    Update boj

---
+++ hiseoung/BOJ/20210929_BOJ_2293_동전1.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def solution(k):
+    '''
+    출처 : https://www.acmicpc.net/problem/2293
+    문제 유형 : 다이내믹 프로그래밍
+    티어 : 실버 1
+    '''
+
+    for i in li:
+        for j in range(i, k + 1):
+            if j - i >= 0:
+                dp[j] += dp[j - i]
+
+    return dp[k]
+
+
+n, k = map(int, sys.stdin.readline().strip().split())
+li = [int(input()) for i in range(n)]
+dp = [0 for i in range(k+1)]
+dp[0] = 1
+print(solution(k))
