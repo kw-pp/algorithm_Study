cc9e097ab90844786e09fc0df61338eacc4f0d87
unknown <anhisg4104@gmail.com>
2021-10-25 23:51:56 +0900

    Add boj_11404

---
+++ hiseoung/BOJ/20210927_BOJ_4948_베르트랑 공준.PY.py
@@ -0,0 +1,37 @@
+import sys
+
+def solution11404():
+    '''
+    출처 : https://www.acmicpc.net/problem/11404
+    분류 : 최단거리-플로이드 와샬
+    시간복잡도 : O(N^3)
+    '''
+    for i in range(n):
+        for a in range(n):
+            for b in range(n):
+                busInfo[a][b] = min(busInfo[a][b], busInfo[a][i] + busInfo[i][b])
+
+    return busInfo
+
+n = int(sys.stdin.readline().strip())
+m = int(sys.stdin.readline().strip())
+busInfo = [[float('inf') for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            busInfo[i][j] = 0
+
+for i in range(m):
+    start, end, edge = map(int, sys.stdin.readline().strip().split())
+    if busInfo[start-1][end-1] != float('inf'):
+        busInfo[start - 1][end - 1] = min(busInfo[start - 1][end - 1], edge)
+    else:
+        busInfo[start-1][end-1] = edge
+
+solution11404()
+
+for i in busInfo:
+    for j in i:
+        print(j, end=' ')
+    print()
