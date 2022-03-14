53f0ebe1c0b9810cb393863b01d73a09d3d6485a
hiseoung <anhisg4104@gmail.com>
2022-03-15 01:34:05 +0900

    Update boj solution

--- hiseoung/BOJ/구현/20210313_boj_14503_로봇청소기.py
+++ hiseoung/BOJ/구현/20210313_boj_14503_로봇청소기.py
@@ -1,45 +1,53 @@
 import sys
 from collections import deque
 
-# def dfs():
-#     for a, b in r_d:
-#         if 0 <= x+a < n and 0 <= y+b < m and table[x+a][y+b] == 0:
-#             table[x+a][y+b] = 2
+
+def dfs(x, y, d):
+    print(0)
+    global flag
+    if flag:
+        print(1)
+        return
+    if d > 3:
+        d = d - 4
+    for _ in range(4):
+        if d > 3:
+            d = d - 4
+        if table[x + r_d[d][0]][y + r_d[d][1]] == 0:
+            print((x + r_d[d][0], y + r_d[d][1]))
+            print("현재 d : ", d)
+            table[x + r_d[d][0]][y + r_d[d][1]] = 2
+            dfs(x + r_d[d][0], y + r_d[d][1], d+1)
+            if table[x + r_d[abs(d - 2)][0]][y + r_d[abs(d - 2)][1]] == 1:
+                if d + 1 > 3:
+                    f = 0
+                else:
+                    f = d + 1
+                if d - 1 < 0:
+                    s = 3
+                else:
+                    s = d - 1
+                if table[x + r_d[f][0]][y + r_d[f][1]] == 2 and table[x + r_d[s][0]][y + r_d[s][1]] == 2:
+                    flag = 1
+                    print(2)
+                    break
+
+        print("d : ", d)
+        print(3)
+        d += 1
+    print(4)
+
+
+
 
 n, m = map(int, sys.stdin.readline().split())
 r, c, d = map(int, sys.stdin.readline().split())
 
 table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# 0 : 북, 1 : 동, 2 : 남, 3: 서
-r_d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-temp = r_d[::-1]
-
-
-if d == 0:
-    r_d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-elif d == 1:
-    r_d = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-elif d == 2:
-    r_d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-else:
-    r_d = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-
 table[r][c] = 2
-q = deque([(r, c)])
-
-
-while q:
-    x,y = q.popleft()
-    for a, b in r_d:
-        if 0 <= x+a < n and 0 <= y+b < m and table[x+a][y+b] == 0:
-            # flag += 1
-            table[x+a][y+b] = 2
-            q.append((x+a, y+b))
+flag = 0
+# 0 : 북, 1 : 동, 2 : 남, 3: 서
+r_d = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+dfs(r, c, d)
 
 print(sum([row.count(2) for row in table]))
-
-
-
-
-
