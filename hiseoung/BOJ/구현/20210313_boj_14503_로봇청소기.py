526dab8e80b38cddd36d138aee51582c3d262419
hiseoung <anhisg4104@gmail.com>
2022-03-17 22:45:02 +0900

    Add boj solution

--- hiseoung/BOJ/구현/20210313_boj_14503_로봇청소기.py
+++ hiseoung/BOJ/구현/20210313_boj_14503_로봇청소기.py
@@ -1,43 +1,15 @@
 import sys
-from collections import deque
 
 
-def dfs(x, y, d):
-    print(0)
-    global flag
-    if flag:
-        print(1)
-        return
-    if d > 3:
-        d = d - 4
-    for _ in range(4):
-        if d > 3:
-            d = d - 4
-        if table[x + r_d[d][0]][y + r_d[d][1]] == 0:
-            print((x + r_d[d][0], y + r_d[d][1]))
-            print("현재 d : ", d)
-            table[x + r_d[d][0]][y + r_d[d][1]] = 2
-            dfs(x + r_d[d][0], y + r_d[d][1], d+1)
-            if table[x + r_d[abs(d - 2)][0]][y + r_d[abs(d - 2)][1]] == 1:
-                if d + 1 > 3:
-                    f = 0
-                else:
-                    f = d + 1
-                if d - 1 < 0:
-                    s = 3
-                else:
-                    s = d - 1
-                if table[x + r_d[f][0]][y + r_d[f][1]] == 2 and table[x + r_d[s][0]][y + r_d[s][1]] == 2:
-                    flag = 1
-                    print(2)
-                    break
-
-        print("d : ", d)
-        print(3)
-        d += 1
-    print(4)
-
-
+def rotate(direction):
+    if direction == 0:
+        return 3
+    elif direction == 1:
+        return 0
+    elif direction == 2:
+        return 1
+    else:
+        return 2
 
 
 n, m = map(int, sys.stdin.readline().split())
@@ -45,9 +17,29 @@ r, c, d = map(int, sys.stdin.readline().split())
 
 table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 table[r][c] = 2
-flag = 0
-# 0 : 북, 1 : 동, 2 : 남, 3: 서
-r_d = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-dfs(r, c, d)
+x = r
+y = c
+r_d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-print(sum([row.count(2) for row in table]))
+while True:
+    flag = 0
+    for _ in range(4):
+        d = rotate(d)
+        if 0 <= x + r_d[d][0] < n and 0 <= y + r_d[d][1] < m and table[x + r_d[d][0]][y + r_d[d][1]] == 0:
+            table[x + r_d[d][0]][y + r_d[d][1]] = 2
+            x = x + r_d[d][0]
+            y = y + r_d[d][1]
+            flag = 1
+            break
+
+    if not flag:
+        x = x - r_d[d][0]
+        y = y - r_d[d][1]
+        if table[x][y] == 1:
+            print(sum([row.count(2) for row in table]))
+            break
+
+
+
+
+
