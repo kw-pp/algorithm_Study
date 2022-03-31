b5c1d18c65e557e82fdde8fe76f4a697f6cea646
Susie <sschoidev@gmail.com>
2022-03-31 20:24:56 +0900

    Update 20220325_1079_마피아.py
    코드 설명 주석 추가

--- susie/BOJ/20220325_1079_마피아.py
+++ susie/BOJ/20220325_1079_마피아.py
@@ -37,20 +37,25 @@ def solution(cnt, day):
 
     #낮
     if day:
-        idx = -1
+        idx = 0
         guilt_point = 0
         for i in range(N):
+            #안죽은 사람들만 유죄 지수 탐색
             if dead[i] == 0:
+                #생존자들 중 가장 큰 유죄 지수를 탐색
                 if guilt[i] > guilt_point:
                     guilt_point = guilt[i]
                     idx = i
+        #내 유죄지수가 생존자들중 가장 큰 경우
         if idx == my_pos:
             return
         
+        #밤으로
         dead[idx] = 1
         day = 0
         dead_cnt += 1
         solution(cnt, day)
+
         dead_cnt -= 1
         day = 1
         dead[idx] = 0
@@ -58,14 +63,19 @@ def solution(cnt, day):
     #밤
     else:
         for i in range(N):
+            #생존자이면서 내가 아닌 다른 사람을 죽일 경우
             if dead[i] == 0 and i != my_pos:
                 dead[i] = 1
+                #죽일 사람의 유죄지수를 더한다
                 for j in range(N):
                     guilt[j] += arr[i][j]
+                
+                #낮으로
                 cnt += 1
                 day = 1
                 dead_cnt += 1
                 solution(cnt, day)
+
                 dead_cnt -= 1
                 dead[i] = 0
                 day = 0
@@ -73,7 +83,6 @@ def solution(cnt, day):
                 for j in range(N):
                     guilt[j] -= arr[i][j]
         
-
 if my_pos != -1:
     solution(0, 0)
 
