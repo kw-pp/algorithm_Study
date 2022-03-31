b24b3ae6aa0e5119632c6b86d3c0a0a5b23d134b
Susie <sschoidev@gmail.com>
2022-03-31 20:11:25 +0900

    Create 20220325_1079_마피아.py
    어려운 문제, 다시보기

---
+++ susie/BOJ/20220325_1079_마피아.py
@@ -0,0 +1,79 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+guilt = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(N)]
+my_pos = int(input())
+dead = [0] * N
+dead_cnt = N
+answer = 0
+
+#전체 인원이 홀수일때 밤에 한명 죽이기 (밤 시작)
+if N % 2 == 1:
+    idx = -1
+    guilt_point = 0
+    for i in range(N):
+        if guilt[i] > guilt_point:
+            guilt_point = guilt[i]
+            idx = i
+    
+    dead[idx] = 1
+    dead_cnt -= 1
+    
+    #마피아에게 내가 암살당할 경우
+    if idx == my_pos:
+        my_pos = -1
+
+def solution(cnt, day):
+    global n,my_pos,answer,dead_cnt
+
+    if cnt > answer:
+      answer = cnt
+    
+    #마지막까지 내가 생존했을 경우
+    if dead_cnt == 1:
+        return
+
+    #낮
+    if day:
+        idx = -1
+        guilt_point = 0
+        for i in range(N):
+            if dead[i] == 0:
+                if guilt[i] > guilt_point:
+                    guilt_point = guilt[i]
+                    idx = i
+        if idx == my_pos:
+            return
+        
+        dead[idx] = 1
+        day = 0
+        dead_cnt -= 1
+        solution(cnt, day)
+        dead_cnt += 1
+        day = 1
+        dead[idx] = 0
+        
+    #밤
+    else:
+        for i in range(N):
+            if dead[i] == 0 and i != my_pos:
+                dead[i] = 1
+                for j in range(N):
+                    guilt[j] += arr[i][j]
+                cnt += 1
+                day = 1
+                dead_cnt -= 1
+                solution(cnt, day)
+                dead_cnt += 1
+                dead[i] = 0
+                day = 0
+                cnt -= 1
+                for j in range(N):
+                    guilt[j] -= arr[i][j]
+        
+if my_pos != -1:
+    solution(0, 0)
+
+print(answer)
