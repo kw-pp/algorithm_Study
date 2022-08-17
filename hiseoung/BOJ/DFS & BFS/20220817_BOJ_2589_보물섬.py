719d73daa87a4cba4aa2fbac7b96496414c4cf5f
hiseoung <anhisg4104@gmail.com>
2022-08-17 15:29:11 +0900

    feat: add boj solution 2589 보물섬

---
+++ hiseoung/BOJ/DFS & BFS/20220817_BOJ_2589_보물섬.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+table = [[*input().strip()] for _ in range(n)]
+max_val = float('-inf')
+
+
+def search_treasure(x, y):
+    q = deque([(x, y)])
+    is_visited = [[False] * m for _ in range(n)]
+    is_visited[x][y] = True
+    count = -1
+    while q:
+        for _ in range(len(q)):
+            n_x, n_y = q.popleft()
+            for a, b in (1, 0), (0, 1), (-1, 0), (0, -1):
+                nx = n_x+a
+                ny = n_y+b
+                if 0 <= nx < n and 0 <= ny < m and not is_visited[nx][ny] and table[nx][ny] == 'L':
+                    is_visited[nx][ny] = True
+                    q.append((nx, ny))
+        count += 1
+    return count
+
+
+for i in range(n):
+    for j in range(m):
+        if table[i][j] == 'L':
+            max_val = max(max_val, search_treasure(i, j))
+print(max_val)
