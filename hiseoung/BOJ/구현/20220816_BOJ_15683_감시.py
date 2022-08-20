775baa60f0f30cfb0b6a615c4d1d6f8e923022fc
hiseoung <anhisg4104@gmail.com>
2022-08-20 18:48:40 +0900

    refactor: boj solution 15683 감시

--- hiseoung/BOJ/구현/20220816_BOJ_15683_감시.py
+++ hiseoung/BOJ/구현/20220816_BOJ_15683_감시.py
@@ -2,84 +2,84 @@ import sys
 from collections import Counter
 input = sys.stdin.readline
 
-# 주어진 방향으로 CCTV 탐색
+# 주어진 방향으로 CCTV 탐색, 벽이 있으면 반환
 def view_cctv(x, y, a, b):
     area = []
     while True:
         x = x + a
         y = y + b
         if 0 <= x < N and 0 <= y < M:
-            if office[x][y] == 6:
+            if office[x][y] == 6: # 벽에 마주친 경우 루프 종료
                 break
             elif office[x][y] == 0:
-                office[x][y] = 7
+                office[x][y] = 7 # 해당 영역에 방문 했다면, 7로 마킹
                 area.append((x, y))
         else:
             break
-    return area
+    return area # DFS 탐색 종료 후 마킹했던 위치를 반환
 
 
-# CCTV가 달려 있는 위치에서 해당 장비의 유형에 따라서 반복문 실행
+# CCTV가 감시할 수 있는 영역을 탐색 (DFS)
 def search_area(cctv, k, n):
     global min_val
+
     # 재귀 종료 조건 : 더 이상 탐색할 CCTV가 남아 있지 않는 경우
     if k == n:
-        val = sum([Counter(row)[0] for row in office])
+        val = sum([Counter(row)[0] for row in office]) # 비어있는 영역 계산
         if val < min_val:
             min_val = val
         return
+
     x, y, t = cctv[k]
+    area = []
+
     if t == 1:
+        # 1번 CCTV -> 네 방향 순회
         for a, b in (1, 0), (0, 1), (-1, 0), (0, -1):
-            area = view_cctv(x, y, a, b)
-            search_area(cctv, k+1, n)
-            for nx, ny in area:
-                office[nx][ny] = 0
+            area.append(view_cctv(x, y, a, b))
+            search_area(cctv, k + 1, n)
+            for pos in area:
+                for nx, ny in pos:
+                    office[nx][ny] = 0
     elif t == 2:
+        # 2번 CCTV -> 두 방향 순회
         for a, b, c, d in (1, 0, -1, 0), (0, 1, 0, -1):
-            area1 = view_cctv(x, y, a, b)
-            area2 = view_cctv(x, y, c, d)
-            search_area(cctv, k+1, n)
-            for nx, ny in area1:
-                office[nx][ny] = 0
-            for nx, ny in area2:
-                office[nx][ny] = 0
+            area.append(view_cctv(x, y, a, b))
+            area.append(view_cctv(x, y, c, d))
+            search_area(cctv, k + 1, n)
+            for pos in area:
+                for nx, ny in pos:
+                    office[nx][ny] = 0
     elif t == 3:
+        # 3번 CCTV -> 네 방향 순회
         for a, b, c, d in (1, 0, 0, 1), (1, 0, 0, -1), (-1, 0, 0, -1), (-1, 0, 0, 1):
-            area1 = view_cctv(x, y, a, b)
-            area2 = view_cctv(x, y, c, d)
-            search_area(cctv, k+1, n)
-            for nx, ny in area1:
-                office[nx][ny] = 0
-            for nx, ny in area2:
-                office[nx][ny] = 0
+            area.append(view_cctv(x, y, a, b))
+            area.append(view_cctv(x, y, c, d))
+            search_area(cctv, k + 1, n)
+            for pos in area:
+                for nx, ny in pos:
+                    office[nx][ny] = 0
     elif t == 4:
+        # 4번 CCTV -> 네 방향 순회
         for a, b, c, d, e, f in (1, 0, 0, 1, 0, -1), (1, 0, 0, -1, -1, 0), (-1, 0, 0, -1, 0, 1), (-1, 0, 0, 1, 1, 0):
-            area1 = view_cctv(x, y, a, b)
-            area2 = view_cctv(x, y, c, d)
-            area3 = view_cctv(x, y, e, f)
-            search_area(cctv, k+1, n)
-            for nx, ny in area1:
-                office[nx][ny] = 0
-            for nx, ny in area2:
-                office[nx][ny] = 0
-            for nx, ny in area3:
-                office[nx][ny] = 0
+            area.append(view_cctv(x, y, a, b))
+            area.append(view_cctv(x, y, c, d))
+            area.append(view_cctv(x, y, e, f))
+            search_area(cctv, k + 1, n)
+            for pos in area:
+                for nx, ny in pos:
+                    office[nx][ny] = 0
     elif t == 5:
+        # 5번 CCTV -> 모든 방향 탐색이므로 한번만 처리
         a, b, c, d, e, f, g, h = 1, 0, -1, 0, 0, 1, 0, -1
-        area1 = view_cctv(x, y, a, b)
-        area2 = view_cctv(x, y, c, d)
-        area3 = view_cctv(x, y, e, f)
-        area4 = view_cctv(x, y, g, h)
-        search_area(cctv, k+1, n)
-        for nx, ny in area1:
-            office[nx][ny] = 0
-        for nx, ny in area2:
-            office[nx][ny] = 0
-        for nx, ny in area3:
-            office[nx][ny] = 0
-        for nx, ny in area4:
-            office[nx][ny] = 0
+        area.append(view_cctv(x, y, a, b))
+        area.append(view_cctv(x, y, c, d))
+        area.append(view_cctv(x, y, e, f))
+        area.append(view_cctv(x, y, g, h))
+        search_area(cctv, k + 1, n)
+        for pos in area:
+            for nx, ny in pos:
+                office[nx][ny] = 0
     return
 
 N, M = map(int, input().split())
