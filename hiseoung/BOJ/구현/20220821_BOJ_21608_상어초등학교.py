27d45d3d140e91b27bf4b74cba3ac07a836a9867
hiseoung <anhisg4104@gmail.com>
2022-08-27 20:20:19 +0900

    refactor: boj solution 21608 상어초등학교

--- hiseoung/BOJ/구현/20220821_BOJ_21608_상어초등학교.py
+++ hiseoung/BOJ/구현/20220821_BOJ_21608_상어초등학교.py
@@ -2,21 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-info = [[*map(int, input().split())] for _ in range(n**2)]
-
+info_list = [*reversed([[*map(int, input().split())] for _ in range(n**2)])]
 table = [[0] * n for _ in range(n)]
 is_visited = [[False] * n for _ in range(n)]
-# 자리를 지정하는 학생의 순서가 정해져 있음 -> 스택 자료구조 활용을 위해 리스트 뒤집기
-info_list = list(reversed(info))
 info_dict = dict()
 answer = 0
 
+# 주어진 학생 정보를 순서대로 탐색 ( 완전 탐색 )
 while info_list:
     student_info = info_list.pop()
     num = student_info[0]
     liked = student_info[1:]
     info_dict[num] = liked
     seat_info = []
+    # 모든 좌석을 탐색하면서 해당 영역에서 발생 할 수 있는 모든 케이스 계산 후 배열 추가
     for i in range(n):
         for j in range(n):
             if not is_visited[i][j]:
@@ -37,12 +36,13 @@ while info_list:
                         elif table[nx][ny] == liked[3]:
                             temp[0] += 1
                 seat_info.append(temp)
+    # 계산을 통해 추가한 모든 후보 좌석에 대해서 조건 기준에 따라서 정렬 후 최적값을 테이블에 갱신
     seat_info.sort(key=lambda x:(x[0], x[1], -x[2], -x[3]))
     seat = seat_info[-1]
     table[seat[2]][seat[3]] = num
     is_visited[seat[2]][seat[3]] = True
 
-
+# 점수 계산
 for i in range(n):
     for j in range(n):
         count = 0
